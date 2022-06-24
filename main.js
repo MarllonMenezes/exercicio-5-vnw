e674375a6bfb4935c7d29e378015e1df8b1ddce7
marllondallas <marllon_dallas@hotmail.com>
2022-06-24 17:46:20 -0300

    exercicio 5 vnw

---
+++ main.js
@@ -0,0 +1,33 @@
+ // Crie um array que receba 5 itens e exiba no console.
+/* let frutas = ['Maçã','Banana','abacaxi','framboesa','uva',];
+
+ console.log(frutas);*/
+
+// Utilize um método para adicionar um nome ao inicio do array.
+/*let letras = ['B','C','D']
+let adicionar = letras.unshift('A')
+console.log(letras); */
+
+// Utilize um método para remover o último nome do array.
+
+/*let letras = ['B','C','D']
+let ultimo = letras.pop()
+console.log(letras); */
+
+// Utilize um método para adicionar dois nomes ao fim do array.
+/*let letras = ['B','C','D']
+let ultimo = letras.push('E','F')
+console.log(letras); */
+
+
+
+// Utilize um método para remover o primeiro nome do array.
+
+/*let letras = ['A','B','C']
+let ultimo = letras.shift('')
+console.log(letras); 
+*/
+
+// Utilize um método para organizar em ordem crescente o seguinte array:
+/* let numbers = [7,5,6,3,8,9,2,1,4]
+console.log (numbers.sort()) */
